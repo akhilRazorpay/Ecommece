3ca65c83f3d80e1ea58bbea965e0cdd16a21ea1b
akhil <gandikota.akhil@razorpay.com>
2022-08-03 15:28:24 +0530

    initiated database

---
+++ database/database.go
@@ -0,0 +1,16 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func GetDB() (db *sql.DB, err error) {
+	dbDriver := "mysql"
+	dbName := "mysql1"
+	dbUser := "admin"
+	dbPass := "1234"
+	db, err = sql.Open(dbDriver, dbUser+": "+dbPass+"@/"+dbName)
+	return
+}
